4eb392421f82b3f2e4939648d0c79c8d6711782c
kentliu <71258048+kentlius@users.noreply.github.com>
2022-04-17 18:41:40 +0700

    add main menu & routing

---
+++ src/klinik_terdekat.py
@@ -0,0 +1,59 @@
+import PySimpleGUI as sg
+
+theme_dict = {'BACKGROUND': '#2B475D',
+                'TEXT': '#FFFFFF',
+                'INPUT': '#F2EFE8',
+                'TEXT_INPUT': '#000000',
+                'SCROLL': '#F2EFE8',
+                'BUTTON': ('#000000', '#C2D4D8'),
+                'PROGRESS': ('#FFFFFF', '#C7D5E0'),
+                'BORDER': 1,'SLIDER_DEPTH': 0, 'PROGRESS_DEPTH': 0}
+
+sg.theme_add_new('Dashboard', theme_dict)
+sg.theme('Dashboard')
+
+BORDER_COLOR = '#C7D5E0'
+DARK_HEADER_COLOR = '#1B2838'
+BPAD_TOP = ((20,20), (20, 10))
+BPAD_LEFT = ((20,10), (0, 10))
+BPAD_LEFT_INSIDE = (0, 10)
+BPAD_RIGHT = ((10,20), (10, 20))
+
+top_banner = [[sg.Text('Clinicare', font='Any 20', background_color=DARK_HEADER_COLOR)]]
+
+top  = [[sg.Text('Klinik Terdekat', font='Any 20', pad=((10,0), (0, 0))),
+         sg.Input('Provinsi', pad=((20, 0),(0, 0)), size=(20,1)),
+         sg.Input('Kota', pad=((20, 0),(0, 0)), size=(20,1)),
+         sg.Button('Cari', pad=((20, 0),(0, 0)), size=(10,1))]]
+
+block_3 = [[sg.Text('Nama Klinik', font='Any 20')],
+            [sg.Text('Alamat Klinik'), sg.Text('Jam Buka & Tutup')],
+            [sg.Button('Daftar')]]
+
+block_2 = [[sg.Text('Nama Klinik', font='Any 20')],
+            [sg.Text('Alamat Klinik'), sg.Text('Jam Buka & Tutup')],
+            [sg.Button('Daftar')]]
+
+block_4 = [[sg.Text('Nama Klinik', font='Any 20')],
+            [sg.Text('Alamat Klinik'), sg.Text('Jam Buka & Tutup')],
+            [sg.Button('Daftar')]]
+
+block_5 = [[sg.Text('Nama Klinik', font='Any 20')],
+            [sg.Text('Alamat Klinik'), sg.Text('Jam Buka & Tutup')],
+            [sg.Button('Daftar')]]
+
+LAYOUT_KLINIK = [[sg.Column(top_banner, size=(960, 40), pad=(0,0), background_color=DARK_HEADER_COLOR)],
+          [sg.Column(top, size=(920, 50), pad=BPAD_TOP)],
+          [sg.Column([[sg.Column(block_2, size=(450,100), pad=BPAD_LEFT_INSIDE)],
+                      [sg.Column(block_3, size=(450,100),  pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT, background_color=BORDER_COLOR),
+           sg.Column([[sg.Column(block_4, size=(450,100), pad=BPAD_LEFT_INSIDE)],
+                      [sg.Column(block_5, size=(450,100), pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT, background_color=BORDER_COLOR)]]
+
+# window = sg.Window('Klinik Terdekat', layout, background_color=BORDER_COLOR)
+
+# while True:
+#     event, values = window.read()
+#     print(event, values)
+#     if event in (sg.WIN_CLOSED, 'Exit'):
+#         break
+# window.close()
--- src/main.py
+++ src/main.py
@@ -1,1 +1,71 @@
-print("Hello World!")
+import PySimpleGUI as sg
+from login import LAYOUT_LOGIN
+from register import LAYOUT_REGISTER
+from klinik_terdekat import LAYOUT_KLINIK
+
+sg.theme('LightGreen3')
+
+BTN_SIZE = (40, 2)
+F_SIZE = (40, 2)
+YELLOW = "#DEBE97"
+TITLE_SIZE = "24px"
+
+LAYOUT_MAIN_BEFORE_LOGIN = [
+    [sg.Text('Main Menu', font=TITLE_SIZE, size=F_SIZE, justification="center")],
+    [sg.Button('Login', key='LOGIN', size=BTN_SIZE)],
+    [sg.Button('Registrasi', key='REGISTER', size=BTN_SIZE)],
+    [sg.Button('Exit', button_color=YELLOW, size=BTN_SIZE)]
+]
+
+LAYOUT_MAIN_AFTER_LOGIN_PASIEN = [
+    [sg.Text('Main Menu Pasien', font=TITLE_SIZE, size=F_SIZE, justification="center")],
+    [sg.Button('Daftar Check Up', key='KLINIK', size=BTN_SIZE)],
+    [sg.Text('')],
+    [sg.Button('Logout', key='LAYOUT_MAIN_BEFORE_LOGIN'), sg.Button('Exit')]
+]
+
+LAYOUT_MAIN_AFTER_LOGIN_DOKTER = [
+    [sg.Text('Main Menu Dokter', font=TITLE_SIZE, size=F_SIZE, justification="center")],
+    [sg.Button('Cek Request', size=BTN_SIZE)],
+    [sg.Text('')],
+    [sg.Button('Logout', key='LAYOUT_MAIN_BEFORE_LOGIN'), sg.Button('Exit')]
+]
+
+LAYOUT = [
+    [
+        sg.Column(LAYOUT_MAIN_BEFORE_LOGIN, key='LAYOUT_MAIN_BEFORE_LOGIN', element_justification='c'),
+        sg.Column(LAYOUT_LOGIN, visible=False, key='LAYOUT_LOGIN', element_justification='c'),
+        sg.Column(LAYOUT_REGISTER, visible=False, key='LAYOUT_REGISTER', element_justification='c'),
+        sg.Column(LAYOUT_MAIN_AFTER_LOGIN_DOKTER, visible=False, key='LAYOUT_MAIN_AFTER_LOGIN_DOKTER', element_justification='c'),
+        sg.Column(LAYOUT_MAIN_AFTER_LOGIN_PASIEN, visible=False, key='LAYOUT_MAIN_AFTER_LOGIN_PASIEN', element_justification='c'),
+        sg.Column(LAYOUT_KLINIK, visible=False, key='LAYOUT_KLINIK')
+    ]
+]
+
+window = sg.Window('Clinicare', LAYOUT)
+
+LAYOUT = 'MAIN_BEFORE_LOGIN'
+
+while True:
+    event, values = window.read()
+    print(event, values)
+
+    if event in ('LOGIN', 'REGISTER', 'KLINIK'):
+        window[f'LAYOUT_{LAYOUT}'].update(visible=False)
+        LAYOUT = event
+        window[f'LAYOUT_{LAYOUT}'].update(visible=True)
+
+    elif 'MAIN_AFTER_LOGIN_' in event:
+        window[f'LAYOUT_{LAYOUT}'].update(visible=False)
+        LAYOUT = event
+        window[f'LAYOUT_{LAYOUT}'].update(visible=True)
+
+    elif 'MAIN_BEFORE_LOGIN' in event:
+        window[f'LAYOUT_{LAYOUT}'].update(visible=False)
+        LAYOUT = 'MAIN_BEFORE_LOGIN'
+        window[f'LAYOUT_{LAYOUT}'].update(visible=True)
+
+    if event == sg.WIN_CLOSED or 'Exit' in event:
+        break
+
+window.close()
