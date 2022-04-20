f8779bcfa0f30caa37fd564b152bb5af322cbbd4
kentliu <71258048+kentlius@users.noreply.github.com>
2022-04-20 16:09:16 +0700

    change delimiter to ,

--- src/klinik_terdekat.py
+++ src/klinik_terdekat.py
@@ -29,7 +29,7 @@ LAYOUT_KLINIK = [
 def load_klinik():
     kliniks = []
     with open('./data/klinik.txt') as klinik_list:
-        klinik = csv.reader(klinik_list, delimiter=';')
+        klinik = csv.reader(klinik_list, delimiter=',')
         for each_klinik in klinik:
             kliniks.append({'nama_klinik': each_klinik[0], 'alamat': each_klinik[1], 'kota': each_klinik[2], 'provinsi': each_klinik[3], 'jam_buka': each_klinik[4], 'jam_tutup': each_klinik[5]})
     return kliniks
