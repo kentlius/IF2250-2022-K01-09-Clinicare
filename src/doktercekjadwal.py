2c1fe6e26b9c2150d3d58defed0fde3ea784747d
OjaanIr <13520067@std.stei.itb.ac.id>
2022-04-20 13:55:28 +0700

    need to handle layout when accepting/rejecting request

--- src/doktercekjadwal.py
+++ src/doktercekjadwal.py
@@ -27,45 +27,79 @@ def delete_request(delete):
         request = csv.writer(request_list, delimiter=",", lineterminator="\n")
         for each_request in requests:
             request.writerow([each_request["username"], each_request["kontak"], each_request["waktu"], each_request["klinik"]])
-    
 
-top_banner = [[sg.Text('Clinicare', font='Any 20', background_color=DARK_HEADER_COLOR)]]
-
-top  = [[sg.Text('Daftar Request', font='Any 20', pad=((10,0), (0, 0)))]]
-
-if len(get_request()) >= 1:
-    block_1 = [[sg.Text(get_request()[0]['username'], font='Any 20')],
+if (len(get_request()) >= 4):
+    LAYOUT_CEK_JADWAL = [
+        [sg.Column([[sg.Text('Clinicare', font='Any 20', background_color=DARK_HEADER_COLOR)]], size=(960, 40), pad=(0,0), background_color=DARK_HEADER_COLOR)],
+        [sg.Column([[sg.Text('Daftar Request', font='Any 20', pad=((10,0), (0, 0)))]], size=(920, 50), pad=BPAD_TOP)],
+        [sg.Column([[sg.Column([[sg.Text(get_request()[0]['username'], font='Any 20')],
                 [sg.Text(get_request()[0]['kontak']), sg.Text(get_request()[0]['waktu']), sg.Text(get_request()[0]['klinik'])],
-                [sg.Button('Terima', key="MAIN_REQUEST1"), sg.Button('Tolak', key="MAIN_REQUEST1")]]
-else:
-    block_1 = [[sg.Text("Tidak ada request", font='Any 20')]]
-
-if len(get_request()) >= 2: 
-    block_2 = [[sg.Text(get_request()[1]['username'], font='Any 20')],
-                [sg.Text(get_request()[1]['kontak']), sg.Text(get_request()[1]['waktu']), sg.Text(get_request()[1]['klinik'])],
-                [sg.Button('Terima', key="MAIN_REQUEST2"), sg.Button('Tolak', key="MAIN_REQUEST2")]]
-else:
-    block_2 = [[sg.Text("Tidak ada request", font='Any 20')]]
-
-if len(get_request()) >= 3:
-    block_3 = [[sg.Text(get_request()[2]['username'], font='Any 20')],
+                [sg.Button('Terima', key="MAIN_CONFIRM"), sg.Button('Tolak', key="MAIN_REJECT")]], size=(450,100), pad=BPAD_LEFT_INSIDE)],
+                [sg.Column([[sg.Text(get_request()[2]['username'], font='Any 20')],
                 [sg.Text(get_request()[2]['kontak']), sg.Text(get_request()[2]['waktu']), sg.Text(get_request()[2]['klinik'])],
-                [sg.Button('Terima', key="MAIN_REQUEST3"), sg.Button('Tolak', key="MAIN_REQUEST3")]]
-else:
-    block_3 = [[sg.Text("Tidak ada request", font='Any 20')]]
-
-if len(get_request()) >= 4:
-    block_4 = [[sg.Text(get_request()[3]['username'], font='Any 20')],
+                [sg.Button('Terima', key="MAIN_CONFIRM"), sg.Button('Tolak', key="MAIN_REJECT")]], size=(450,100),  pad=BPAD_LEFT_INSIDE)]], 
+                pad=BPAD_LEFT, background_color=BORDER_COLOR),
+        sg.Column([[sg.Column([[sg.Text(get_request()[1]['username'], font='Any 20')],
+                [sg.Text(get_request()[1]['kontak']), sg.Text(get_request()[1]['waktu']), sg.Text(get_request()[1]['klinik'])],
+                [sg.Button('Terima', key="MAIN_CONFIRM"), sg.Button('Tolak', key="MAIN_REJECT")]], size=(450,100), pad=BPAD_LEFT_INSIDE)],
+                [sg.Column([[sg.Text(get_request()[3]['username'], font='Any 20')],
                 [sg.Text(get_request()[3]['kontak']), sg.Text(get_request()[3]['waktu']), sg.Text(get_request()[3]['klinik'])],
-                [sg.Button('Terima', key="MAIN_REQUEST4"), sg.Button('Tolak', key="MAIN_REQUEST4")]]
-else:
-    block_4 = [[sg.Text("Tidak ada request", font='Any 20')]]
-
-
-LAYOUT_CEK_JADWAL = [[sg.Column(top_banner, size=(960, 40), pad=(0,0), background_color=DARK_HEADER_COLOR)],
-          [sg.Column(top, size=(920, 50), pad=BPAD_TOP)],
-          [sg.Column([[sg.Column(block_1, size=(450,100), pad=BPAD_LEFT_INSIDE)],
-                      [sg.Column(block_2, size=(450,100),  pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT, background_color=BORDER_COLOR),
-           sg.Column([[sg.Column(block_3, size=(450,100), pad=BPAD_LEFT_INSIDE)],
-                      [sg.Column(block_4, size=(450,100), pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT, background_color=BORDER_COLOR)],
-                      [sg.Button('Back', key='MAIN_AFTER_LOGIN_DOKTER')]]
+                [sg.Button('Terima', key="MAIN_CONFIRM"), sg.Button('Tolak', key="MAIN_REJECT")]], size=(450,100), pad=BPAD_LEFT_INSIDE)]], 
+                pad=BPAD_LEFT, background_color=BORDER_COLOR)],
+        [sg.Button('Back', key='MAIN_AFTER_LOGIN_DOKTER')]
+    ]
+elif (len(get_request()) == 3):
+    LAYOUT_CEK_JADWAL = [
+        [sg.Column([[sg.Text('Clinicare', font='Any 20', background_color=DARK_HEADER_COLOR)]], size=(960, 40), pad=(0,0), background_color=DARK_HEADER_COLOR)],
+        [sg.Column([[sg.Text('Daftar Request', font='Any 20', pad=((10,0), (0, 0)))]], size=(920, 50), pad=BPAD_TOP)],
+        [sg.Column([[sg.Column([[sg.Text(get_request()[0]['username'], font='Any 20')],
+                [sg.Text(get_request()[0]['kontak']), sg.Text(get_request()[0]['waktu']), sg.Text(get_request()[0]['klinik'])],
+                [sg.Button('Terima', key="MAIN_CONFIRM"), sg.Button('Tolak', key="MAIN_REJECT")]], size=(450,100), pad=BPAD_LEFT_INSIDE)],
+                [sg.Column([[sg.Text(get_request()[2]['username'], font='Any 20')],
+                [sg.Text(get_request()[2]['kontak']), sg.Text(get_request()[2]['waktu']), sg.Text(get_request()[2]['klinik'])],
+                [sg.Button('Terima', key="MAIN_CONFIRM"), sg.Button('Tolak', key="MAIN_REJECT")]], size=(450,100),  pad=BPAD_LEFT_INSIDE)]], 
+                pad=BPAD_LEFT, background_color=BORDER_COLOR),
+        sg.Column([[sg.Column([[sg.Text(get_request()[1]['username'], font='Any 20')],
+                [sg.Text(get_request()[1]['kontak']), sg.Text(get_request()[1]['waktu']), sg.Text(get_request()[1]['klinik'])],
+                [sg.Button('Terima', key="MAIN_CONFIRM"), sg.Button('Tolak', key="MAIN_REJECT")]], size=(450,100), pad=BPAD_LEFT_INSIDE)],
+                [sg.Column([[sg.Text("Tidak ada request", font='Any 20')]], size=(450,100), pad=BPAD_LEFT_INSIDE)]], 
+                pad=BPAD_LEFT, background_color=BORDER_COLOR)],
+        [sg.Button('Back', key='MAIN_AFTER_LOGIN_DOKTER')]
+    ]
+elif (len(get_request()) == 2):
+    LAYOUT_CEK_JADWAL = [
+        [sg.Column([[sg.Text('Clinicare', font='Any 20', background_color=DARK_HEADER_COLOR)]], size=(960, 40), pad=(0,0), background_color=DARK_HEADER_COLOR)],
+        [sg.Column([[sg.Text('Daftar Request', font='Any 20', pad=((10,0), (0, 0)))]], size=(920, 50), pad=BPAD_TOP)],
+        [sg.Column([[sg.Column([[sg.Text(get_request()[0]['username'], font='Any 20')],
+                [sg.Text(get_request()[0]['kontak']), sg.Text(get_request()[0]['waktu']), sg.Text(get_request()[0]['klinik'])],
+                [sg.Button('Terima', key="MAIN_CONFIRM"), sg.Button('Tolak', key="MAIN_REJECT")]], size=(450,100), pad=BPAD_LEFT_INSIDE)],
+                [sg.Column([[sg.Text("Tidak ada request", font='Any 20')]], size=(450,100),  pad=BPAD_LEFT_INSIDE)]], 
+                pad=BPAD_LEFT, background_color=BORDER_COLOR),
+        sg.Column([[sg.Column([[sg.Text(get_request()[1]['username'], font='Any 20')],
+                [sg.Text(get_request()[1]['kontak']), sg.Text(get_request()[1]['waktu']), sg.Text(get_request()[1]['klinik'])],
+                [sg.Button('Terima', key="MAIN_CONFIRM"), sg.Button('Tolak', key="MAIN_REJECT")]], size=(450,100), pad=BPAD_LEFT_INSIDE)],
+                [sg.Column([[sg.Text("Tidak ada request", font='Any 20')]], size=(450,100), pad=BPAD_LEFT_INSIDE)]], 
+                pad=BPAD_LEFT, background_color=BORDER_COLOR)],
+        [sg.Button('Back', key='MAIN_AFTER_LOGIN_DOKTER')]
+    ]
+elif (len(get_request()) == 1):
+    LAYOUT_CEK_JADWAL = [
+        [sg.Column([[sg.Text('Clinicare', font='Any 20', background_color=DARK_HEADER_COLOR)]], size=(960, 40), pad=(0,0), background_color=DARK_HEADER_COLOR)],
+        [sg.Column([[sg.Text('Daftar Request', font='Any 20', pad=((10,0), (0, 0)))]], size=(920, 50), pad=BPAD_TOP)],
+        [sg.Column([[sg.Column([[sg.Text(get_request()[0]['username'], font='Any 20')],
+                [sg.Text(get_request()[0]['kontak']), sg.Text(get_request()[0]['waktu']), sg.Text(get_request()[0]['klinik'])],
+                [sg.Button('Terima', key="MAIN_CONFIRM"), sg.Button('Tolak', key="MAIN_REJECT")]], size=(450,100), pad=BPAD_LEFT_INSIDE)],
+                [sg.Column([[sg.Text("Tidak ada request", font='Any 20')]], size=(450,100),  pad=BPAD_LEFT_INSIDE)]], 
+                pad=BPAD_LEFT, background_color=BORDER_COLOR),
+        sg.Column([[sg.Column([[sg.Text("Tidak ada request", font='Any 20')]], size=(450,100), pad=BPAD_LEFT_INSIDE)],
+                [sg.Column([[sg.Text("Tidak ada request", font='Any 20')]], size=(450,100), pad=BPAD_LEFT_INSIDE)]], 
+                pad=BPAD_LEFT, background_color=BORDER_COLOR)],
+        [sg.Button('Back', key='MAIN_AFTER_LOGIN_DOKTER')]
+    ]
+elif (len(get_request()) == 0):
+    LAYOUT_CEK_JADWAL = [
+        [sg.Column([[sg.Text('Clinicare', font='Any 20', background_color=DARK_HEADER_COLOR)]], size=(960, 40), pad=(0,0), background_color=DARK_HEADER_COLOR)],
+        [sg.Column([[sg.Text('Daftar Request', font='Any 20', pad=((10,0), (0, 0)))]], size=(920, 50), pad=BPAD_TOP)],
+        [sg.Column([[sg.Text("Tidak ada request", font='Any 20', justification="center")]])],
+        [sg.Button('Back', key='MAIN_AFTER_LOGIN_DOKTER')]
+    ]
